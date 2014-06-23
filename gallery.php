93de04791e4595affa40a5c98220e1592d399475
Martin Holzhauer <martin@holzhauer.eu>
2014-06-23 14:15:29 +0200

    upps

--- gallery.php
+++ gallery.php
@@ -1,9 +1,9 @@
 <?php
 
 echo '<ul>';
-foreach((new DirectoryIterator(__DIR__.'/img')) as $i) {
+foreach((new DirectoryIterator(__DIR__.'/img/')) as $img) {
     if ($img->isFile()) {
-        echo '<li><img src="img/'.$i->getBasename().'"></li>';
+        echo '<li><img src="img/'.$img->getBasename().'"></li>';
     }
 }
 echo '</ul>';
